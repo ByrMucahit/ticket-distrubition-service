e9bd293fd749ab077d6f525ecfba5214e940cbd7
ByrMucahit <m.mucahitbayar@gmail.com>
2025-03-28 23:01:41 +0300

    feat: "update validate-travel.guard.ts to validate travel"

---
+++ src/guard/validate-travel.guard.ts
@@ -0,0 +1,20 @@
+import { CanActivate, ExecutionContext, Injectable, BadRequestException } from '@nestjs/common';
+import { TravelsService } from '../travel/travels.service';
+import { TRAVEL_STATUSES } from '../constants/travel.constants';
+
+@Injectable()
+export class ValidateTravelGuard implements CanActivate {
+  constructor(private travelService: TravelsService) {}
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    try {
+      const request = context.switchToHttp().getRequest();
+      const travelId = request.body.travel_id || request.param || request.query || request.headers['travel_id'];
+      const travel = await this.travelService.findTravelById(travelId, { status: 1 });
+      if (travel && travel.status === TRAVEL_STATUSES.ACTIVE) return true;
+      throw new BadRequestException(`Invalid Travel === Travel id: ${travelId}`);
+    } catch (ex: any) {
+      throw new BadRequestException(`${ex}`);
+    }
+  }
+}
