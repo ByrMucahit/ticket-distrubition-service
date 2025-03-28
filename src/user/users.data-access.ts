bb40426e04fc14b0ce5ed3eab3e772e2ae27b3fe
ByrMucahit <m.mucahitbayar@gmail.com>
2025-03-28 23:00:20 +0300

    feat: "add users.data-access.ts to build a layer to make communcation with database by using typeorm"

--- src/user/users.data-access.ts
+++ src/user/users.data-access.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { UsersEntity } from './users.entity';
 import { Repository } from 'typeorm';
+import { CreateUserDto } from './dto/create-user.dto';
 
 @Injectable()
 export class UsersDataAccess {
@@ -9,5 +10,11 @@ export class UsersDataAccess {
 
   async findUserById(id: string): Promise<UsersEntity | null> {
     return this.usersRepository.findOne({ where: { id } });
+  }
+
+  async createUser(createUserDto: CreateUserDto): Promise<UsersEntity> {
+    const userEntity = this.usersRepository.create(createUserDto);
+    userEntity.updated_at = new Date();
+    return this.usersRepository.save(userEntity);
   }
 }
