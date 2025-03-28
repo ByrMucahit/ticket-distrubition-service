608e3d6384c699b1e9c2c76f34ea8b9286b1eebe
ByrMucahit <m.mucahitbayar@gmail.com>
2025-03-28 22:43:32 +0300

    feat: "update app.module.ts to inject modules"

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,13 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DataSourceModule } from './database/datasource';
 import { UsersModule } from './user/users.module';
+import { TicketModule } from './ticket/ticket.module';
+import { TravelsModule } from './travel/travels.module';
+import { Gateway } from './gateway/gateway';
 
 @Module({
-  imports: [DataSourceModule, UsersModule],
+  imports: [DataSourceModule, UsersModule, TicketModule, TravelsModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, Gateway],
 })
 export class AppModule {}
