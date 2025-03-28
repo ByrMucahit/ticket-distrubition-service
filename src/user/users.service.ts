096ff1627bf8d569c8b1a56c05066e539fd4f2f1
ByrMucahit <m.mucahitbayar@gmail.com>
2025-03-28 23:01:10 +0300

    feat: "update users.service.ts to add new method to create users"

--- src/user/users.service.ts
+++ src/user/users.service.ts
@@ -1,12 +1,23 @@
 import { Injectable } from '@nestjs/common';
 import { UsersDataAccess } from './users.data-access';
 import { UsersEntity } from './users.entity';
+import { CreateUserDto } from './dto/create-user.dto';
+import { Gateway } from '../gateway/gateway';
 
 @Injectable()
 export class UsersService {
-  constructor(private usersDataAccess: UsersDataAccess) {}
+  constructor(
+    private usersDataAccess: UsersDataAccess,
+    private gateway: Gateway,
+  ) {}
 
   async findUserById(id: string): Promise<UsersEntity | null> {
     return this.usersDataAccess.findUserById(id);
+  }
+
+  async createUser(createUserDto: CreateUserDto): Promise<UsersEntity> {
+    const user = await this.usersDataAccess.createUser(createUserDto);
+    this.gateway.sendToAll(user.firstname);
+    return user;
   }
 }
