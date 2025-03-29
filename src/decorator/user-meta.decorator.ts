87fb2aadc8d1894570fc89a5a9e5d47d88a2367c
ByrMucahit <m.mucahitbayar@gmail.com>
2025-03-29 17:08:34 +0300

    feat: "update user-meta.decorator.ts to create new interface for user meta"

--- src/decorator/user-meta.decorator.ts
+++ src/decorator/user-meta.decorator.ts
@@ -1,5 +1,9 @@
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 
+export interface UserMetaI {
+  user_id: Uuid;
+}
+
 export function UserMeta() {
   return createParamDecorator((_: unknown, ctx: ExecutionContext): { user_id: Uuid } => {
     const request = ctx.switchToHttp().getRequest();
