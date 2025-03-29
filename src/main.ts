a790f0a544687fe51589dbe9c08dd83f4c377af1
ByrMucahit <m.mucahitbayar@gmail.com>
2025-03-29 17:04:16 +0300

    feat: "update main.ts to refine cors issue"

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ALLOWED_ORIGINS_ARRAY } from './constants/origin';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const corsOptions = {
-    origin: (origin, callback) => {
-      if (ALLOWED_ORIGINS_ARRAY.includes(origin) || !origin) {
-        callback(null, true);
-      } else {
-        callback(new Error('Origin not allowed by CORS'));
-      }
-    },
+    origin: true,
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     preflightContinue: false,
     optionsSuccessStatus: 204,
@@ -26,4 +19,4 @@ async function bootstrap() {
 
   await app.listen(process.env.PORT ?? 3000);
 }
-bootstrap();
+void bootstrap();
